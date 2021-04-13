d1d52404be38692871481263df27514751032d4b
Yosef <shmuli5770@gmail.com>
2021-04-13 18:03:40 -0400

    pasted in script and HTML from pair session

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let prioritySelector = document.querySelector("#priority")
+let form = document.querySelector("#create-task-form")
+let theList = document.querySelector("#tasks")
+let priorityHigh = 'High', priorityMedium = 'Medium', priorityLow = 'Low'
+form.addEventListener("submit", (iceCream) => {
+   iceCream.preventDefault()
+   let result = iceCream.target["new-task-description"].value
+   let newTask = document.createElement("li")
+   let deleteBtn = document.createElement("button")
+   let chosenPriority = iceCream.target['priority-list'].value
+   deleteBtn.innerText = 'X'
+   newTask.innerText = `${result} `
+   newTask.style.color = chosenPriority
+   newTask.append(deleteBtn)
+   deleteBtn.addEventListener('click', () => {
+      newTask.remove()
+   })
+   theList.append(newTask)
+})
